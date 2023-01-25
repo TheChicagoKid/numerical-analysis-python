0532f1aff76e7225b257c770c1404b1cf35546f1
Ty Rodriguez <ty.rodriguez2021@gmail.com>
2023-01-25 14:16:03 -0600

    Add methods to solve triangular matrices

---
+++ matrix/solve_lower_triangular.py
@@ -0,0 +1,21 @@
+def solve_lower(A, b):
+    """
+    Solves the equation Ax = b where x and b are vectors (python lists) and A
+    is a lower triangular matrix (python list of lists)
+    Args:
+        A: Lower triangular matrix (Ax = b)
+        b: Right hand side vector (Ax = b)
+    Returns:
+        The vector x that we are solving for (Ax = b)
+    """
+    n = len(b)
+    x = [0 for _ in range(n)]
+
+    for i in range(n):
+        x[i] = b[i]
+        for j in range(i):
+            print(j)
+            x[i] -= A[i][j] * x[j]
+        x[i] /= A[i][i]
+
+    return x
---
+++ matrix/solve_upper_triangular.py
@@ -0,0 +1,21 @@
+def solve_upper(A, b):
+    """
+    Solves the equation Ax = b where x and b are vectors (python lists) and A
+    is a upper triangular matrix (python list of lists)
+    Args:
+        A: Upper triangular matrix (Ax = b)
+        b: Right hand side vector (Ax = b)
+    Returns:
+        The vector x that we are solving for (Ax = b)
+    """
+    n = len(b)
+    x = [0 for _ in range(n)]
+
+    for i in range(n - 1, -1, -1):
+        x[i] = b[i]
+        for j in range(n - 1, i, -1):
+            print(j)
+            x[i] -= A[i][j] * x[j]
+        x[i] /= A[i][i]
+
+    return x
